ae270ee27831587f3bbf63937e0597f7ff35a672
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-02-25 17:51:03 +0900

    Added now deployment

---
+++ .next/static/webpack/static/development/pages/index.js.f7bc76a1c8710d54b52f.hot-update.js
@@ -0,0 +1,75 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Layout */ "./components/Layout.tsx");
+var _jsxFileName = "/Users/ivanvanderbyl/dev/ivanvanderbyl/dev/pages/index.tsx";
+ // import Link from "next/link";
+
+
+
+var IndexPage = function IndexPage() {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"](_components_Layout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    title: "Ivan Vanderbyl",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, "Hi \uD83D\uDC4B"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "This is Ivan Vanderbyl. I'm the a Product Manager at", " ", react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("a", {
+    href: "https://tricentis.com",
+    target: "_blank",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Tricentis"), " ", "working on Load Testing."), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "Previously, I co-founded Flood IO", react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("i", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, " (Acq. Tricentis, June 2017)"), " and worked as a Software Engineer at", " ", react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("a", {
+    href: "https://digitalocean.com",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "DigitalOcean"), " in the early days."));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (IndexPage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f7bc76a1c8710d54b52f.hot-update.js.map
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import * as React from "react";
+// import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage: React.FunctionComponent = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
+    <Layout title="Ivan Vanderbyl">
+      <h1>Hi 👋</h1>
+      <p>
+        This is Ivan Vanderbyl. I'm the a Product Manager at{" "}
+        <a href="https://tricentis.com" target="_blank">
+          Tricentis
+        </a>{" "}
+        working on Load Testing.
+      </p>
+      <p>
+        Previously, I co-founded Flood IO
+        <i> (Acq. Tricentis, June 2017)</i> and worked as a Software Engineer at{" "}
+        <a href="https://digitalocean.com">DigitalOcean</a> in the early days.
+      </p>
+      <p>
+        Over the last 12+ years I've founded 4 companies, exited 2, crashed and
+        burned 1, and fizzled out another.
+      </p>
+      <p>
+        I've worked in Product related roles including Engineering, Design, and
+        Management. I enjoy the challenges of pushing products forward and
+        challenging what is technically possible.
+      </p>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
